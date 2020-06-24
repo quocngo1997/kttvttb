033de7d98bc9d054b13d7204517c615028e4754c
Quoc Ngo <quocnv@minori.com.vn>
2020-06-24 17:45:10 +0700

    kttvttb

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function clickShowMenu() {
+  var element = document.getElementById("nav-side");
+  element.classList.add("in");
+}
+function clickHideMenu() {
+  var element = document.getElementById("nav-side");
+  element.classList.remove("in");
+}
